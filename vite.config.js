36b0af6055aff0b5fd6d86a53bdd8e1968c4d239
btopro <bto108@psu.edu>
2024-09-25 16:31:26 -0400

    https://github.com/haxtheweb/issues/issues/2152 start

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'loading.html'),
+      },
+    },
+  },
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
