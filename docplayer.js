2fc4f6b5b3a4d8ad63459dad88aecc09ad7c3593
btopro <bto108@psu.edu>
2025-09-03 15:21:53 -0400

    https://github.com/haxtheweb/issues/issues/2395

--- docplayer.js
+++ docplayer.js
@@ -1,4 +1,5 @@
 import "./style.css";
 import "@haxtheweb/web-container/lib/web-container-wc-registry-docs.js";
 let wc = document.createElement('web-container-wc-registry-docs');
+wc.text = 'Select the project to produce a demo for and doc player will make a best attempt at loading. Most elements should load from the registry but some only make sense in a large application context. To learn more about loading registry elements anywhere see the "Magic" link.';
 document.querySelector('#app').appendChild(wc);
